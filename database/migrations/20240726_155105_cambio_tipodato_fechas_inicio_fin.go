c107d6811c64fab65f759da01e34571c48f85656
Juan Castro <juan941223@gmail.com>
2024-08-30 16:23:08 -0500

    fix: re seralizan ajustes varios para preparacion de despliegue udistrital/sisifo_documentacion#151

--- database/migrations/20240726_155105_cambio_tipodato_fechas_inicio_fin.go
+++ database/migrations/20240726_155105_cambio_tipodato_fechas_inicio_fin.go
@@ -1,10 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"strings"
-
 	"github.com/astaxie/beego/migration"
 )
 
@@ -24,7 +20,8 @@ func init() {
 // Run the migrations
 func (m *CambioTipodatoFechasInicioFin_20240726_155105) Up() {
 	// use m.SQL("CREATE TABLE ...") to make schema update
-	file, err := os.ReadFile("../scripts/20240726_155105_cambio_tipodato_fechas_inicio_fin.up.sql")
+	//se remueve ya que las migraciones se haran de manera manual y asi evitar conflictos
+	/*file, err := os.ReadFile("../scripts/20240726_155105_cambio_tipodato_fechas_inicio_fin.up.sql")
 
 	if err != nil {
 		// handle error
@@ -37,13 +34,14 @@ func (m *CambioTipodatoFechasInicioFin_20240726_155105) Up() {
 		fmt.Println(request)
 		m.SQL(request)
 		// do whatever you need with result and error
-	}
+	}*/
 }
 
 // Reverse the migrations
 func (m *CambioTipodatoFechasInicioFin_20240726_155105) Down() {
 	// use m.SQL("DROP TABLE ...") to reverse schema update
-	file, err := os.ReadFile("../scripts/20240726_155105_cambio_tipodato_fechas_inicio_fin.down.sql")
+	//se remueve ya que las migraciones se haran de manera manual y asi evitar conflictos
+	/*file, err := os.ReadFile("../scripts/20240726_155105_cambio_tipodato_fechas_inicio_fin.down.sql")
 
 	if err != nil {
 		// handle error
@@ -56,5 +54,5 @@ func (m *CambioTipodatoFechasInicioFin_20240726_155105) Down() {
 		fmt.Println(request)
 		m.SQL(request)
 		// do whatever you need with result and error
-	}
+	}*/
 }
