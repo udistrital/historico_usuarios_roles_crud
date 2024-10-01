760d96cac0ac2c90e86d90473e859123a2dcecd2
Heiner Edwar Granados Leal <50187485+edwargl7@users.noreply.github.com>
2024-10-01 10:13:06 -0500

    feat: Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ set -u
 set -o pipefail
 
 if [ -n "${PARAMETER_STORE:-}" ]; then
-  export USUARIO_ROL_PGuser="$(aws ssm get-parameter --name /${PARAMETER_STORE}/historico_rol_usuario_crud/db/username --output text --query Parameter.Value)"
-  export USUARIO_ROL_PGpass="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/historico_rol_usuario_crud/db/password --output text --query Parameter.Value)"
+  export USUARIO_ROL_PGuser="$(aws ssm get-parameter --name /${PARAMETER_STORE}/usuario_rol_crud/db/username --output text --query Parameter.Value)"
+  export USUARIO_ROL_PGpass="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/usuario_rol_crud/db/password --output text --query Parameter.Value)"
 fi
 
 exec ./main "$@"
